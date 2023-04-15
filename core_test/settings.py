27b46be7bca689af957528ecacf9f2162213f5e1
Ivystu <ivystu0315@gmail.com>
2023-04-16 03:31:15 +0800

    修改setting的installed_app

--- core_test/settings.py
+++ core_test/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dcp_test.apps.DcpTestConfig',
-    'dcp_test'
+    'dcp_test.apps.DcpTestConfig'
 ]
 
 MIDDLEWARE = [
