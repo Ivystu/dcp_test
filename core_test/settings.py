6298e4ee1391c0e2f8a529603ed5bcc3c5cbc2d9
Ivystu <ivystu0315@gmail.com>
2023-04-16 03:33:26 +0800

    修改static路徑

--- core_test/settings.py
+++ core_test/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = './dcp_test/static/'
+STATIC_URL = '/dcp_test/static/'
 
 STATICFILES_DIRS = os.path.join(STATIC_ROOT, 'static')
 
